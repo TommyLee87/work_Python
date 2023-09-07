a87663e38226cff5aceccffac1a1a489bc96fc34
TommyLee <kokokorkr@naver.com>
2023-09-07 17:54:02 +0900

    반복문 / 별찍기, 사각형

--- 반복문_0907.py
+++ 반복문_0907.py
@@ -138,8 +138,8 @@
 
 # n = int(input("별 갯수 입력 : "))
 # for i in range(n, 0, -1):
-#     for s in range(n - 1):
-#         print(" ", end=" ")
+#     for s in range(n - i):
+#         print(" ", end="")
 #     for j in range(i):
 #         print("*", end=" ")
 #     print()
