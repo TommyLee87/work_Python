979467de38794037a24b3118651770dcd5a8a6aa
TommyLee <kokokorkr@naver.com>
2023-09-12 15:23:29 +0900

    연산자오버로딩

--- 연산자오버로딩_0912.py
+++ 연산자오버로딩_0912.py
@@ -4,7 +4,7 @@ class Vector2D:
         self.y = y
 
     def __add__(self, other):  # (+) 연산에 대응됩니다.
-        return Vector2D(self.x + other.x, self.y + other.y)
+        return Vector2D(self.x + other, self.y + other)
 
     def __mul__(self, other):
         return Vector2D((self.x * other.x) + 100, (self.y * other.y) + 100)
@@ -16,4 +16,5 @@ v2 = Vector2D(3, 4)
 print(100 * 200)
 print(100.1 * 200.1)
 v3 = v1 * v2
-print(v3.x, v3.y)
+v4 = v1 + 100
+print(v4.x, v4.y)
