4fae614836d5c14f6081634f28e57909467ed3ce
TommyLee <kokokorkr@naver.com>
2023-09-12 12:17:07 +0900

    파일 입출력

--- 파일입출력_0912.py
+++ 파일입출력_0912.py
@@ -14,7 +14,7 @@
 # readline() : 해당 파일의 내용을 한 줄씩 읽고 문자열로 반환, 더 이상 내용이 없으면 None
 # readlines() : 해당 파일의 모든 내용을 순서대로 읽어 각각의 라인을 하나의 요소로 저장하는 리스트 반환
 
-score_file = open("score.txt", "r", encoding="utf-8")
+# score_file = open("score.txt", "r", encoding="utf-8")
 # print(score_file.read())
 # score_file.close()
 # while True:
@@ -24,10 +24,11 @@ score_file = open("score.txt", "r", encoding="utf-8")
 #     print(line, end="")  # 한 줄씩 읽고 출력하기 때문에 줄바꿈 문자까지 포함되어 있음.
 # score_file.close()
 #
-lines = score_file.readlines()  # 해당 파일의 모든 라인을 순서대로 읽고 리스트 생성
-for e in lines:
-    print(e, end="")
-score_file.close()
+# lines = score_file.readlines()  # 해당 파일의 모든 라인을 순서대로 읽고 리스트 생성
+# for e in lines:
+#     print(e, end="")
+# print(lines)
+# score_file.close()
 
 # with 키워드 사용하기 : open() 이후에 자동으로 close를 호출해 주는 기능
 # with open("score.txt", "r", encoding="utf-8") as score_file:
@@ -48,4 +49,15 @@ from datetime import datetime
 #         #             nav               5                     0                      0                !     LSU
 #         print("비밀번호 : " + password)
 #         fd.write(password + "\n")
-# fd.close()
+
+import pickle
+# 객체를 직렬화하여 파일에 저장하기
+# data = {"name": "곰돌이사육사", "age": 22, "addr": "서울시 강남구 역삼동"}
+# with open("data.pickle", "wb") as file:
+#     pickle.dump(data,file)
+
+# 파일에서 객체를 역직렬화하여 복원하기
+with open("data.pickle", "rb") as file:
+    restored_data = pickle.load(file)
+
+print(restored_data)
