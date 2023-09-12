0325b4c64fbd374e43a71cec563c6d2589af8d9c
TommyLee <kokokorkr@naver.com>
2023-09-12 15:47:06 +0900

    정적메소드

--- 정적메소드_0912.py
+++ 정적메소드_0912.py
@@ -12,7 +12,8 @@ class Car :
         self.speed = speed
         print(f"{self.size} & {self.model} 자동차가 시속 {self.speed}km로 달립니다.")
 
-    @staticmethod  # 클래스 소속, 객체로 만들어지지 않음
+
+    # 클래스 소속, 객체로 만들어지지 않음
     def check_type(code):
         if(code >= 10 and code < 20): print("전기차 입니다.")
         elif(code >= 20 and code < 30): print("가솔린차 입니다.")
@@ -21,7 +22,6 @@ class Car :
 
 car1 = Car("소형", "모닝")
 car2 = Car("중형", "쏘나타")
-
 car1.move(90)
 Car.check_type(20)
 car2.move(70)
